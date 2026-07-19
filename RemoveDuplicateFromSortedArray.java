75faaa695f03950a32275bc4ef27b865a8b17f91
MUNI SOMESH <someshmuni7@gmail.com>
2026-07-20 00:47:24 +0530

    Implemented duplicate removal from sorted array

--- RemoveDuplicateFromSortedArray.java
+++ RemoveDuplicateFromSortedArray.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import java.util.Arrays;
 
 class Solution {
@@ -45,52 +44,4 @@ public class RemoveDuplicateFromSortedArray {
 
         System.out.println("Complete array: " + Arrays.toString(nums));
     }
-=======
-import java.util.Arrays;
-
-class Solution {
-
-    public int RemoveDuplicateFromSortedArray(int[] nums) {
-
-        if (nums.length == 0) {
-            return 0;
-        }
-
-        int i = 0;
-
-        for (int j = 1; j < nums.length; j++) {
-
-            if (nums[i] != nums[j]) {
-                i++;
-                nums[i] = nums[j];
-            }
-        }
-
-        return i + 1;
-    }
-}
-
-public class RemoveDuplicateFromSortedArray {
-
-    public static void main(String[] args) {
-
-        Solution obj = new Solution();
-
-        int[] nums = {1, 1, 2, 2, 3, 4, 4};
-
-        int k = obj.RemoveDuplicateFromSortedArray(nums);
-
-        System.out.println("Number of unique elements: " + k);
-
-        System.out.print("Array after removing duplicates: ");
-
-        for (int i = 0; i < k; i++) {
-            System.out.print(nums[i] + " ");
-        }
-
-        System.out.println();
-
-        System.out.println("Complete array: " + Arrays.toString(nums));
-    }
->>>>>>> f0bc46c (Moved Java programs from day folders to main folder)
 }
